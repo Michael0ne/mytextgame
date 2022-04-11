e2ff06d230518553f1ce5baa8f67e04225244906
Michael0ne <misha.belov96@gmail.com>
2022-04-11 19:30:40 +0300

    Project files added.

---
+++ MyTextGame.cpp
@@ -0,0 +1,12 @@
+﻿// MyTextGame.cpp : Defines the entry point for the application.
+//
+
+#include "MyTextGame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
